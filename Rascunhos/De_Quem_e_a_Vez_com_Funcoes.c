3b095a4831ec88df7d1226c5f8cb9fdcb5d611b9
JeanMoutinho98 <142322746+JeanMoutinho98@users.noreply.github.com>
2023-10-03 11:15:58 -0300

    Add files via upload

---
+++ Rascunhos/De_Quem_e_a_Vez_com_Funcoes.c
@@ -0,0 +1,25 @@
+#include <stdio.h>                                                  //Completar
+#include <string.h>
+
+int main(){
+    int entradas, valor1, valor2;
+    char nome1[101], nome2[101], escolha1[6], escolha2[6];
+
+
+    scanf("%d", &entradas);
+
+
+    for(int i = 0; i < entradas; i++){
+        scanf("%s", nome1);
+        scanf("%s", escolha1);
+        scanf("%s", nome2);
+        scanf("%s", escolha2);
+        scanf("%d", &valor1);
+        scanf("%d", &valor2);
+        int soma = valor1 + valor2;
+        char SomaEhParOuImpar; 
+        if(strcmp(ehParOuImpar, escolha1) ==)
+    }
+
+return 0;
+}
